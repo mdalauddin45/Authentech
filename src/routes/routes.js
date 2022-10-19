e502dccfd7f56ec79af51dee73ae60f882817119
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-19 21:05:35 +0600

    fdg

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,9 @@ import Main from "../layout/Main";
 import ErrorPage from "../components/ErrorPage";
 import Login from "../components/Login";
 import Home from "../components/Home";
+import Register from "../components/Register";
+import Wallet from "../components/Wallet";
+import Profile from "../components/Profile";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +16,9 @@ const router = createBrowserRouter([
       { path: "/", element: <Home></Home> },
       { path: "/home", element: <Home></Home> },
       { path: "/login", element: <Login></Login> },
+      { path: "/register", element: <Register></Register> },
+      { path: "/wallet", element: <Wallet></Wallet> },
+      { path: "/profile", element: <Profile></Profile> },
     ],
   },
 ]);
