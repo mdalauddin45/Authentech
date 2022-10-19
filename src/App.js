bbb3ff65a15d343960b1dc1d41599b5cbe1d5323
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-19 20:55:30 +0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./routes/routes";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold ">Hello world!</h1>
-    </div>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
