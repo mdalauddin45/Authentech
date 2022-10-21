a7689478df3a537909dad418fc44737b45e574fc
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-21 11:52:46 +0600

    fgs

--- src/index.js
+++ src/index.js
@@ -9,11 +9,10 @@ import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <UserContext>
     <ToastContainer position="top-center" />
-    <UserContext />
     <App />
-  </React.StrictMode>
+  </UserContext>
 );
 
 // If you want to start measuring performance in your app, pass a function
